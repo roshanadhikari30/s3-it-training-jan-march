de6122c2989c763eb3477eeecfea3218ebdee9a8
Sandeep Shahi <sandeepshahi@Sandeeps-MBP.attlocal.net>
2021-01-09 13:05:44 -0600

    tests

--- src/jan09/HelloWorld.java
+++ src/jan09/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World ! Welcome to java session");
+        System.out.println("We are learning GIT");
 
     }
 }
