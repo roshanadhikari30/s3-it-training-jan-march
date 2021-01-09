2cf672d27e4bc436f816a634784638bf4a19c7c0
Sandeep Shahi <sandeepshahi@Sandeeps-MBP.attlocal.net>
2021-01-09 13:16:36 -0600

    added some lines of code

--- src/jan09/HelloWorld.java
+++ src/jan09/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World ! Welcome to java session");
         System.out.println("We are learning GIT");
+        System.out.println("we have lerned GIT");
 
     }
 }
