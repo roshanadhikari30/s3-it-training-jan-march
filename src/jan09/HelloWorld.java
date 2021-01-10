2dc65967572aeaea2a55b23b06d1d02d0e625f53
Kailash Dhakal <dhakalkailash1997@gmail.com>
2021-01-10 12:15:19 -0600

    Name Added

--- src/jan09/HelloWorld.java
+++ src/jan09/HelloWorld.java
@@ -3,7 +3,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World ! Welcome to java session");
+        System.out.println("Hello World ! Its Kaialsh Dhakal");
         System.out.println("We are learning GIT");
         System.out.println("we have lerned GIT");
        System.out.println("Shiddarth Bista");
