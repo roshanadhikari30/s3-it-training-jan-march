bd9bdd37777b7f1bf79a0682ce4d67650345073d
rbasnet1222 <BasicBoat198>
2021-01-16 15:27:13 -0600

    commited my program

---
+++ src/jan09/Car.java
@@ -0,0 +1,11 @@
+package jan09;
+
+public class Car {
+
+public static void main (String[] args) {
+    String car = "BMW";
+    int price = 50000;
+    System.out.println(car);
+}
+
+}
---
+++ src/jan09/House.java
@@ -0,0 +1,9 @@
+package jan09;
+
+public class House {
+    public static void main(String[] args){
+
+        String house = "Townhomes";
+        System.out.println(house);
+    }
+}
