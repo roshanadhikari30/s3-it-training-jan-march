94ce75638493236e761199a84d23a981b1eb892f
shahisandeep <shahi.sandeep3@gmail.com>
2021-01-24 13:45:15 -0600

    Arrays

--- src/jan23/Arrays.java
+++ src/jan23/Arrays.java
@@ -12,24 +12,12 @@ public class Arrays {
     int [] myArray;
      */
 
-    public static void main(String[] args) {
+    public void printArrays(){
         int[] myArray = new int[5];
         System.out.println("Length of the array is " + myArray.length);
-        int lastIndex = myArray.length - 1;
-        System.out.println("Value at index: " + lastIndex + " is " + myArray[lastIndex]);
-        System.out.println("Value at index: 2 is " + myArray[2]);
-        //adding the value
-        //assign a value to an index 4
         myArray[4] = 20;
-        System.out.println(myArray[4]);
-
-        //readability
         for (int printArray : myArray) {
             System.out.println(printArray);
-        }
-
-        for (int x = 0; x < myArray.length; x++) {
-            System.out.println(myArray[x]);
         }
     }
 }
