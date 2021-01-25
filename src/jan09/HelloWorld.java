5d445f1adf3acebf54b66879488f5da7e1b05188
Monika <Monikathapa47@gmail.com>
2021-01-25 17:09:15 -0600

    cleaning and adding my name

--- src/jan09/HelloWorld.java
+++ src/jan09/HelloWorld.java
@@ -1,13 +1,9 @@
-``package jan09;
+package jan09;
 
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World ! Welcome to java session");
-        System.out.println("We are learning GIT");
-        System.out.println("we have lerned GIT");
-       System.out.println("Shiddarth Bista");
-
+        System.out.println("Hello World ! Welcome to Monika's place");
 
     }
 }
