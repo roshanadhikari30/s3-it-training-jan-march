59e3471d5afd97fd41f46bef667f5b88ef9c9504
renushrestha <renustha18@gmail.com>
2021-01-26 18:18:58 -0600

    Finished coding for checking if Check If Leap Year

---
+++ src/if_condition/CheckIfLeapYear.java
@@ -0,0 +1,12 @@
+package if_condition;
+
+
+public class CheckIfLeapYear {
+    public static void main(String[] args) {
+        LeapYear leapYear = new LeapYear();
+
+        System.out.println(leapYear.checkIfLeapYear(2000));
+    }
+
+
+}
