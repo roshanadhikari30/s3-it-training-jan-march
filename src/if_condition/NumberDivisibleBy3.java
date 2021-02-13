06404b197598cf87e08c795ed5e9e2042f464b63
renushrestha <renustha18@gmail.com>
2021-02-13 12:12:31 -0600

    If Condition

---
+++ src/if_condition/NumberDivisibleBy3.java
@@ -0,0 +1,13 @@
+package if_condition;
+
+public class NumberDivisibleBy3 {
+    public static void main(String[] args) {
+        int num = 15;
+
+        if(num % 3 == 0){
+            System.out.println(num + "is divisible by 3");
+        }else{
+            System.out.println(num + "is not divisible by 3");
+        }
+    }
+}
